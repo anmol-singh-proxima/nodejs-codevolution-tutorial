e098a8ebf0ba8dd18fc427212abf22d9aaddfab3
anmol-singh-proxima <kumaranmol888@gmail.com>
2023-06-09 00:29:11 +0530

    Modules: Import and Export Formats

--- index.js
+++ index.js
@@ -5,12 +5,17 @@
 // console.log("Sum:", sum(10,20))
 // console.log("Sum:", sum(100,200))
 
-const Avenger = require('./avenger')
+// Module Caching
+// const Avenger = require("./avenger");
+// const natasha = new Avenger("Natasha Romanoff");
+// console.log(natasha.getName());
+// natasha.setName("Black Widow");
+// console.log(natasha.getName());
+// const wanda = new Avenger("Scarlett Witch");
+// console.log(wanda.getName());
 
-const natasha = new Avenger("Natasha Romanoff")
-console.log(natasha.getName())
-natasha.setName("Black Widow")
-console.log(natasha.getName())
-
-const wanda = new Avenger("Scarlett Witch")
-console.log(wanda.getName())
+// Module Import Patterns
+const math = require("./math");
+console.log(math.add(20, 10));
+console.log(math.sub(20, 10));
+console.log(math.mul(20, 10));
---
+++ math.js
@@ -0,0 +1,14 @@
+// Module Exports Patterns
+const add = (a, b) => {
+  return a + b;
+};
+const sub = (a, b) => {
+  return a - b;
+};
+const mul = (a, b) => {
+  return a * b;
+};
+module.exports = { add, sub, mul };
+// exports.add = add;
+// exports.sub = sub;
+// exports.mul = mul;
